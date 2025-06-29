8e3b35b9892ce7774bed595b08fd1604a6b95a36
Hanan Mohamud <hananmohamudy@gmail.com>
2025-06-29 22:33:23 +0300

    updated puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
-file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-  match  => '^(\s*)PasswordAuthentication\s+',
-}
-
 file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
-  match  => '^(\s*)IdentityFile\s+',
+  match  => '^\s*IdentityFile\s+',
+}
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  match  => '^\s*PasswordAuthentication\s+',
 }
